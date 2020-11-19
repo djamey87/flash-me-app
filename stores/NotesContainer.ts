7d47865c04dbc1dd81e446dd4838896ce0c10a70
djamey <djamey87@gmail.com>
2020-11-19 20:23:07 +0000

    :sparkles: local storage hook added for notes state

--- stores/NotesContainer.ts
+++ stores/NotesContainer.ts
@@ -1,5 +1,6 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { createContainer } from "unstated-next";
+import useStorage from '../hooks/useStorage';
 
 export interface Note {
     initial: string,
@@ -7,11 +8,18 @@ export interface Note {
 }
 
 const useNotes = () =>{
-    let [notes, setNotes] = useState([{}] as Note[]);
+    const [storageValue, updateStorage] = useStorage('@test_key', {notes: [] as Note[]});
+    const [notes, setNotes] = useState([{}] as Note[]);
 
-    let addNote = (newNote:Note) => {
-        console.log("incoming note!", newNote);
-        setNotes([...notes, newNote])
+    useEffect(() => {
+        // console.log('NotesContainer', storageValue);
+        setNotes(storageValue.notes as Note[] || []);
+    }, [storageValue]);
+
+    const addNote = (newNote:Note) => {
+        const concatNotes = [...notes, newNote];
+        setNotes(concatNotes);
+        updateStorage({notes:concatNotes as Note[]});
     }
     return { notes, addNote }
 }
