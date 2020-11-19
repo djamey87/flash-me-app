3742e1f35011b1cf4310b3d55a1d816802c0ffc4
djamey <djamey87@gmail.com>
2020-11-19 20:31:22 +0000

    :mute: cleaning up dev logs

--- hooks/useStorage.ts
+++ hooks/useStorage.ts
@@ -11,15 +11,15 @@ const useAsyncStorage = <T>(key: string, defaultValue: T): [T, (newValue: T) => 
   async function pullFromStorage() {
     const fromStorage = await AsyncStorage.getItem(key)
     let value = defaultValue
+    
     if (fromStorage) {
       value = JSON.parse(fromStorage)
     }
-    console.log('getting storage!', value);
+
     setState({ hydrated: true, storageValue: value });
   }
 
   async function updateStorage(newValue: T) {
-      console.log('incoming storag item', newValue);
     setState({ hydrated: true, storageValue: newValue })
     const stringifiedValue = JSON.stringify(newValue);
     await AsyncStorage.setItem(key, stringifiedValue);
@@ -32,31 +32,4 @@ const useAsyncStorage = <T>(key: string, defaultValue: T): [T, (newValue: T) => 
   return [storageValue, updateStorage, hydrated];
 };
 
-export default useAsyncStorage
-
-
-// import { useState, useEffect } from "react";
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-
-
-// const useAsyncStorage = (key: string, initialValue:object) =>{
-//   const [storedValue, setStoredValue] = useState(initialValue);
-//   useEffect(() => {
-//     AsyncStorage.getItem(key)
-//       .then(value => {
-//         if (value === null) return initialValue;
-//         return JSON.parse(value);
-//       })
-//       .then(setStoredValue)
-//   }, [key, initialValue]);
-
-//   const setValue = (value:{}) => {
-//     const valueToStore = value instanceof Function ? value(storedValue) : value;
-//     setStoredValue(valueToStore);
-//     AsyncStorage.setItem(key, JSON.stringify(valueToStore));
-//   }
-
-//   return [storedValue, setValue];
-// }
-
-// export default useAsyncStorage;
+export default useAsyncStorage;
