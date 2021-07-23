36cbddea9ade5226d166dd6401badc5a34b65766
djamey <djamey87@gmail.com>
2021-07-23 07:20:13 +0100

    :truck: separating styles

--- screens/RevisionScreen/index.tsx
+++ screens/RevisionScreen/index.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState, createRef, useRef } from 'react';
+import React from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 
 import FlippableCard from '../../components/Cards/FlippableCard';
 
-import GestureFlippableCard from '../../components/Cards/GestureFlippableCard';
-
 import NotesContainer, { Note } from '../../stores/NotesContainer';
+
+import styles from './styles';
 
 export default function TabTwoScreen() {
   const { notes } = NotesContainer.useContainer();
@@ -15,45 +15,10 @@ export default function TabTwoScreen() {
       <ScrollView style={styles.scrollContainer}>
 
         {notes.map((note: Note, index: number) => {
-
-
-          // return <FlippableCard key={'card-' + note.id} cardId={note.id} backContent={note.backContent} frontContent={note.frontContent} />
-
           return <FlippableCard key={'card-' + note.id} cardId={note.id} backContent={note.backContent} frontContent={note.frontContent} />
-          // return (
-          //   <CardFlip
-          //     style={styles.cardContainer}
-          //     key={'card-' + index + note.id}
-          //     ref={el => itemsRef.current[index] = el}>
-          //     <TouchableOpacity
-          //       activeOpacity={1}
-          //       style={[styles.card, styles.cardFront]}
-          //       onPress={onPressHandler}>
-          //       <Text style={styles.label}>{note.frontContent}</Text>
-          //     </TouchableOpacity>
-          //     <TouchableOpacity
-          //       activeOpacity={1}
-          //       style={[styles.card, styles.cardBack]}
-          //       onPress={onPressHandler}>
-          //       <Text style={[styles.label, { color: '#000' }]}>{note.backContent}</Text>
-          //     </TouchableOpacity>
-          //   </CardFlip>
-          // );
         })}
 
       </ScrollView>
     </SafeAreaView>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
-    width: '100%'
-  },
-  scrollContainer: {
-    width: '100%'
-  }
-});
---
+++ screens/RevisionScreen/styles.ts
@@ -0,0 +1,19 @@
+import { ViewStyle } from 'react-native';
+
+const container: ViewStyle = {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%'
+};
+
+
+const scrollContainer: ViewStyle = {
+    width: '100%'
+};
+
+export default {
+    container,
+    scrollContainer
+}
